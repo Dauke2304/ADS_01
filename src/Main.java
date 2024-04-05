e1e929461b3d01e5bd78124992aa819e0d31b3fb
20fut <231612@astanait.edu.kz>
2024-04-05 11:51:33 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static First first = new First();
     public static void main(String[] args) {
-        int[] arr={1,2,3,0,5};
-        First first=new First();
+        int n= sc.nextInt();
+        int[] arr=Create_Arr(n);
         System.out.println(first.min_in_array(arr,arr.length-1));
 
+    }
+    public static int[] Create_Arr(int n){
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        return arr;
     }
 }
